3c74f27b2f072ba2b7271b0dae8f7085d37067b5
schmitt <clarisse.schmitt@outlook.com>
2021-05-10 13:31:42 -0700

    first commit

---
+++ app/models/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+    def home 
+    
+    end
+
+    def accueil
+    
+    end
+
+    def team
+        
+    end
+
+    def contact
+    
+    end
+
+end
