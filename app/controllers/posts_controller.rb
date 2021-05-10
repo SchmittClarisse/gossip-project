442e60d240b68b5c54d8184c875f9d843e92622b
schmitt <clarisse.schmitt@outlook.com>
2021-05-10 16:01:50 -0700

    commit 2

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,5 @@
+class PostsController < ApplicationController
+  def index
+    @posts = post.all 
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/home', to: 'pages#home'
   root to: 'pages#home' 
   get '/accueil', to: 'pages#accueil'
   get '/team', to: 'pages#team'
   get '/contact', to: 'pages#contact'
+  get '/articles', to: 'posts#index', as: 'posts'
 end
